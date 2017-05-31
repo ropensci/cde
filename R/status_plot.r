c31bae3f4e850a67f31f8b741d8a25f75c1bd1fa
Rob Briers <r.briers@napier.ac.uk>
2017-05-31 14:54:44 +0100

    starting plotting code

---
+++ R/status_plot.r
@@ -0,0 +1,11 @@
+#' Plot Status Summary
+#' @description Produces a stacked proportional barplot of waterbody
+#' status information for a given set of data (MC, OC or RBD)
+
+#' @inheritParams wfd_class
+#' 
+status_plot<-function(){
+  
+  # sum_data<-with(class_df, table(Status, Year))
+  # barplot(sum_data)
+}
