0b1865abbf268b905c9f8f4130d1081240a93bab
Rob Briers <r.briers@napier.ac.uk>
2019-02-08 21:33:08 +0000

    adding package docs

---
+++ R/cde.r
@@ -0,0 +1,18 @@
+#' cde: Extract and plot Water Framework Directive (WFD) waterbody status data
+#' from the Environment Agency Catchment Data Explorer (CDE) website
+#'
+#' The cde package allows searching, download and plotting of the WFD
+#' waterbody status classification data for all waterbodies within the
+#' Environment Agency area.
+#'
+#' The website that is accessed is:
+#' (http://environment.data.gov.uk/catchment-planning/).
+
+#' The data are made available under the Open Government Licence v3.0
+#' (https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/)
+#'
+#' @importFrom graphics barplot
+#' @docType package
+#'
+#' @name cde
+NULL
