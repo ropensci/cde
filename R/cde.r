727373e6ca12c1fdb5a6423ae49647a733561873
Rob Briers <r.briers@napier.ac.uk>
2019-02-22 18:45:33 +0000

    updating cde description

--- R/cde.r
+++ R/cde.r
@@ -1,13 +1,15 @@
-#' cde: Extract and plot Water Framework Directive (WFD) waterbody status and 
-#' Reasons for Not Achieving Good (RNAG) data from the Environment Agency 
-#' Catchment Data Explorer (CDE) website.
+#' cde: Extract Water Framework Directive (WFD) data from the 
+#' Environment Agency Catchment Data Explorer (CDE) website.
 #'
-#' The cde package allows searching, download and plotting of the WFD
-#' waterbody status classification data and download of RNAG data for all 
-#' waterbodies within the Environment Agency area.
+#' The cde package facilitates searching and download of the WFD-related
+#' data for all waterbodies within the Environment Agency area i.e. England.
+#' The types of data that can be downloaded are: WFD status classification data
+#' (which can also be plotted), Reasons for Not Achieving Good (RNAG) status, 
+#' objectives set for waterbodies, measures put in place to improve water 
+#' quality and details of associated protected areas. 
 #'
-#' The website that is accessed is:
-#' (http://environment.data.gov.uk/catchment-planning/).
+#' The website that is accessed is: 
+#' http://environment.data.gov.uk/catchment-planning/.
 
 #' The data are made available under the Open Government Licence v3.0
 #' (https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/)
